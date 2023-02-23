0eb148972dda7db1c53522b6cdda74e0b1dcbd9c
Thalles Rodrigues <ts.rodr@gmail.com>
2023-02-23 19:29:49 -0300

    modal looks like a modal

--- frontend/src/components/NewGeneratorModal.svelte
+++ frontend/src/components/NewGeneratorModal.svelte
@@ -20,8 +20,7 @@
             closeModal();
         };
     }
-</script>
-
+/*
 <div class="fixed top-0 left-0 w-full h-screen">
     <div class="backdrop-opacity-30 bg-black absolute w-full h-full">
         <div class="bg-bg1 col-span-4 flex-1 flex-col m-4 p-4 place-content-center shadow-xl text-fg">
@@ -31,5 +30,17 @@
             <button class="bg-bg2 p-2 ml-2 text-fg hover:bg-bg3 hover:fg-fg0 transition duration-400" type="submit" on:click={createAndClose} >Create New Generator</button>
             <button class="bg-bg2 p-2 ml-2 text-red hover:bg-bg3 hover:fg-fg0 transition duration-400" type="submit" on:click={closeModal} >X</button>
         </div>
+    </div>
+</div>
+*/
+</script>
+
+<div class="fixed top-0 left-0 flex justify-center items-center opacity-90 bg-black w-screen h-screen">
+    <div class="bg-bg1 p-2 m-2 flex flex-col relative justify-center max-w-md shadow-xl text-fg">
+        <button class="absolute top-0 right-0 bg-bg2 p-2 text-red hover:bg-bg3 hover:fg-fg0 transition duration-400" type="submit" on:click={closeModal} >X</button>
+        <h2 class="text-center text-green p-2 m-2">New Generator</h2>
+        <p class="p-2 m-2 opacity-100">Type in a name for the new generator and click the Confirm button, or click the <span class="text-red">X</span> to cancel.</p>
+        <input class="bg-bg2 p-2 m-2 text-center no-spinner text-yellow" type="text" placeholder="generator name" bind:value={generatorNameInput} />
+        <button class="bg-bg2 p-2 m-2 text-fg hover:bg-bg3 hover:fg-fg0 transition duration-400" type="submit" on:click={createAndClose} >Confirm</button>
     </div>
 </div>
