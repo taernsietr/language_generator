507ca3975e7072c0e179cfef7e00cc219a42a2f7
Thalles Rodrigues <ts.rodr@gmail.com>
2023-02-24 00:19:55 -0300

    added docker/flyio files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
                     .route("/save", web::post().to(save_generator))
             )
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("[::]", 8080))?
     .run()
     .await
 }
