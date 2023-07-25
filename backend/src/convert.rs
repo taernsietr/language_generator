8d2180114e0649ef9cf488f5efb69f5d9fb32c73
Thalles Rodrigues <ts.rodr@gmail.com>
2023-07-25 08:45:53 -0300

    working on convert regex

--- backend/src/convert.rs
+++ backend/src/convert.rs
@@ -1,18 +1,28 @@
 use bimap::BiMap;
-// use regex::{Regex, RegexSet, escape};
+use regex::Regex;
 
-/*
-fn escape_regex(regex: String) {
-    let re = Regex::new(r"[\/{}[]().*<>]").unwrap();
-    re.replace_all(regex
+fn escape_regex(regex: &mut str) -> String {
+    let re = Regex::new(r"([\\/\{\}\[\]\(\)\.\*\?])").unwrap();
+    re.replace_all(regex, r"\$1").to_string()
 }
-*/
 
-// check current symbol with the following symbols, continue if the added symbols are
-// valid xsampa. If a symbol that cannot be parsed as part of a xsampa representation or
-// a suprasegmental, convert the sequence to IPA
 pub fn xsampa_to_ipa(input: String, table: &BiMap<String, String>) -> String {
-    table.get_by_left(&input).unwrap().to_string()
+    let mut result = input.clone();
+    let mut regexes = Vec::<Regex>::new();
+    
+    for each in table.left_values() {
+        // println!("Escaping regex: {}", &each);
+        let mut esc = each.clone();
+        esc = escape_regex(esc.as_mut_str());
+        // println!("Escaped regex: {}", &esc);
+        regexes.push(Regex::new(&esc).unwrap());
+    }
+
+    for each in regexes {
+        result = each.replace_all(&result, "000").to_string();
+    }
+
+    result
 }
 
 pub fn ipa_to_xsampa(input: String, table: &BiMap<String, String>) -> String {
